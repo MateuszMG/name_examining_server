f6a53170b8d4355580eed50b1a3eb9b7996ff5c7
Mateusz MG <MateuszMG.default@com.com>
2023-07-21 10:13:40 +0200

    fix: add name correction

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 import { config } from './config';
 
-export const mongooseConnect = async () => {
+export const mongooseConnection = async () => {
   mongoose.set('strictQuery', false);
   mongoose.connect(config.DATABASE_URL).then(
     () => console.log('Mongoose connected'),
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
 import { config } from './config/config';
-import { mongooseConnect } from './config/db';
+import { mongooseConnection } from './config/db';
 
 import { app } from './app';
 
 app.listen({ port: config.PORT }, () => {
-  mongooseConnect();
-  console.log('Port: ' + config.PORT);
+  mongooseConnection();
+  console.log('Server running on port: ' + config.PORT);
 });
